6dc85e31a4fc64e73a9ecdde812ff2bd780df5a7
Imran Merchant <imerchant@outlook.com>
2023-09-17 19:59:35 -0500

    Adjusted layout breakpoints, add composedOf display in checklist

--- src/StarfieldRecipes/Configuration/Resource.cs
+++ src/StarfieldRecipes/Configuration/Resource.cs
@@ -7,4 +7,5 @@ public class Resource
     public string? Symbol { get; init; }
     public Rarity Rarity { get; init; }
     public Dictionary<string, int>? ComposedOf { get; init; }
+    public bool IsManufactured => ComposedOf?.Count > 0;
 }
